537157784912afa29b64345ad6f872ab32e05a13
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-07-01 15:40:29 +0530

    Updated image src

--- script.js
+++ script.js
@@ -7,51 +7,51 @@ const closeBtn = document.getElementById('close');
 
 const data = [
     {
-        image: './img/drink.jpg',
+        image: 'https://images.unsplash.com/photo-1526034332220-067b0f400e06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=406&q=80',
         text: "I'm Thirsty"
     },
     {
-        image: './img/food.jpg',
+        image: 'https://images.unsplash.com/photo-1529973565457-a60a2ccf750d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
         text: "I'm Hungry"
     },
     {
-        image: './img/tired.jpg',
+        image: 'https://images.unsplash.com/photo-1470777639313-60af88918203?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
         text: "I'm Tired"
     },
     {
-        image: './img/hurt.jpg',
+        image: 'https://images.unsplash.com/photo-1609840534277-88833ef3ddeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
         text: "I'm Hurt"
     },
     {
-        image: './img/happy.jpg',
+        image: 'https://images.unsplash.com/photo-1524117074681-31bd4de22ad3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
         text: "I'm Happy"
     },
     {
-        image: './img/angry.jpg',
+        image: 'https://images.unsplash.com/photo-1503525537183-c84679c9147f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
         text: "I'm Angry"
     },
     {
-        image: './img/sad.jpg',
+        image: 'https://images.unsplash.com/photo-1597176116047-876a32798fcc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
         text: "I'm Sad"
     },
     {
-        image: './img/scared.jpg',
+        image: 'https://images.unsplash.com/photo-1565239500265-bd02167453ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
         text: "I'm Scared"
     },
     {
-        image: './img/outside.jpg',
+        image: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
         text: 'I Want To Go Outside'
     },
     {
-        image: './img/home.jpg',
+        image: 'https://images.unsplash.com/photo-1449844908441-8829872d2607?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
         text: 'I Want To Go Home'
     },
     {
-        image: './img/school.jpg',
+        image: 'https://images.unsplash.com/photo-1588075592446-265fd1e6e76f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
         text: 'I Want To Go To School'
     },
     {
-        image: './img/grandma.jpg',
+        image: 'https://images.unsplash.com/photo-1593100126453-19b562a800c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=867&q=80',
         text: 'I Want To Go To Grandmas'
     }
 ];
